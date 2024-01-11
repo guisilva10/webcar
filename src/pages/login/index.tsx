760e6f5bc90f63ab8de21e19e246b63e5bbb91f6
Guilherme Lopes <guisilva.070104@gmail.com>
2024-01-11 16:48:36 -0300

    feat: initial creating login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,24 @@
+import logoImg from "../../assets/logo.svg"
+import { Container } from "../../components/container"
+import { Link } from "react-router-dom"
+ 
+
+
 export function Login (){
     return (
-        <h1>Login Page</h1>
+        <Container>
+            <div className="w-full min-h-screen justify-center items-center flex flex-col gap-4">
+                <Link to="/" className="mb-6 max-w-sm w-full">
+                    <img src={logoImg} alt="logo do site" className="w-full"/>
+                </Link>
+            </div>
+
+
+            <form 
+             className="w-full mx-w-xl rounded-lg bg-white"
+            >
+
+            </form>
+        </Container>
     )
 }
