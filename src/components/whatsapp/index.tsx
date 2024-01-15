89b68065ae8ea94ab564d11200866496c675d53a
Guilherme Lopes <guisilva.070104@gmail.com>
2024-01-15 15:23:57 -0300

    feat: edit btn suport

--- src/components/whatsapp/index.tsx
+++ src/components/whatsapp/index.tsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 
 export function Whatsapp(){
     return(
-        <div    className="fixed bottom-8 right-8 bg-green-500 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-full inline-flex items-center z-50">
+        <div    className="fixed bottom-8 right-8 bg-green-500 hover:bg-green-600 text-white font-bold py-4 px-4 rounded-lg inline-flex items-center z-50">
             <a 
             className="flex justify-center items-center gap-2 font-medium text-xl"
             href="https://api.whatsapp.com/send?phone=5511948798912&text=Ola, venho da WebCarros, poderia me ajudar ?"
